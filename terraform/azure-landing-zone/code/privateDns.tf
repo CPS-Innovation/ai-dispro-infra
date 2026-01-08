cae51761fca1cce94c6a078e9b15ac4da05f076d
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-01-08 12:49:58 +0000

    Adding More Azure Resources

---
+++ terraform/azure-landing-zone/code/privateDns.tf
@@ -0,0 +1,71 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Private DNS Resources for Key Vault
+
+# -----------------------------------------
+# Private DNS Zone for Key Vault
+# -----------------------------------------
+resource "azurerm_private_dns_zone" "keyvault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# -----------------------------------------
+# Link Key Vault DNS Zone to VNET
+# -----------------------------------------
+resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_vnet_link" {
+  name                  = "pdz-keyvault-vnet-link-${var.subscription}-01"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.keyvault.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Private DNS Resources for PostgreSQL
+
+# -----------------------------------------
+# Private DNS Zone for PostgreSQL
+# -----------------------------------------
+resource "azurerm_private_dns_zone" "psql" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# -----------------------------------------
+# Link PostgreSQL DNS Zone to VNET
+# -----------------------------------------
+resource "azurerm_private_dns_zone_virtual_network_link" "psql_link" {
+  name                  = "pdz-psql-link-${var.subscription}-01"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.psql.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Private DNS Resources for App Services
+
+# -----------------------------------------
+# Private DNS Zone for App Services
+# -----------------------------------------
+resource "azurerm_private_dns_zone" "app" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# -----------------------------------------
+# Link DNS Zone to VNET
+# -----------------------------------------
+resource "azurerm_private_dns_zone_virtual_network_link" "app_vnet_link" {
+  name                  = "pdz-app-vnet-link-${var.subscription}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.app.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
---
+++ terraform/azure-landing-zone/code/storageAccount.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_account" "sa" {
+  name                     = "saaidisprologs${var.subscription}01"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
