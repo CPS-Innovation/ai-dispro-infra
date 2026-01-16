be7ae11cf225dc55dbb43b8716102c58d7d887e6
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-01-16 09:15:00 +0000

    Adding Workloads

--- terraform/azure-workloads/code/tags.tf
+++ terraform/azure-workloads/code/tags.tf
@@ -1,9 +1,11 @@
 module "tags" {
-  source        = "git::https://dev.azure.com/CPSDTS/Case%20Progression/_git/tf-module-common-tags?ref=main"
+  source = "../../modules/common-tags"
+
   pipeline_name = var.pipeline_name
   repo_name     = var.repo_name
   branch_name   = var.branch_name
   repo_uri      = var.repo_uri
   date          = var.date
+  run_number    = var.run_number
   environment   = var.environment
 }
