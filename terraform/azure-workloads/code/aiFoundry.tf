837c4f9c22b9eac9abde55030517f3e429f3674f
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-01-19 17:16:21 +0000

    Adding AI Services

--- terraform/azure-workloads/code/aiFoundry.tf
+++ terraform/azure-workloads/code/aiFoundry.tf
@@ -8,5 +8,5 @@ resource "azurerm_ai_services" "foundry" {
 resource "azurerm_ai_foundry_project" "foundry_project" {
   name               = "fproj-aid-${var.environment}-01"
   location           = azurerm_resource_group.rg.location
-  ai_services_hub_id = azurerm_ai_foundry.foundry.id
+  ai_services_hub_id = azurerm_ai_services.foundry.id
 }
