da522b6e23d1eea31aa529de58e618c83e22f724
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-01-23 09:06:42 +0000

    Adding Private DNS Zones

--- terraform/azure-landing-zone/code/privateDns.tf
+++ terraform/azure-landing-zone/code/privateDns.tf
@@ -75,3 +75,51 @@ resource "azurerm_private_dns_zone_virtual_network_link" "app_vnet_link" {
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------
+# Private DNS Zone for Storage Blob
+# -----------------------------------------
+resource "azurerm_private_dns_zone" "storage_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = module.tags.keyvalues
+}
+
+# -----------------------------------------
+# Link Storage Blob DNS Zone to VNET
+# -----------------------------------------
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_blob_vnet_link" {
+  name                  = "pdz-storage-blob-vnet-link-${var.subscription}-01"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_blob.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+  tags                  = module.tags.keyvalues
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------
+# Private DNS Zone for Storage Table
+# -----------------------------------------
+resource "azurerm_private_dns_zone" "storage_table" {
+  name                = "privatelink.table.core.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = module.tags.keyvalues
+}
+
+# -----------------------------------------
+# Link Storage Table DNS Zone to VNET
+# -----------------------------------------
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_table_vnet_link" {
+  name                  = "pdz-storage-table-vnet-link-${var.subscription}-01"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_table.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+  tags                  = module.tags.keyvalues
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
--- terraform/azure-workloads/code/storageAccount.tf
+++ terraform/azure-workloads/code/storageAccount.tf
@@ -12,3 +12,29 @@ resource "azurerm_storage_container" "corpus" {
   storage_account_id    = azurerm_storage_account.aidds_sa.id
   container_access_type = "private"
 }
+
+resource "azurerm_private_endpoint" "aidds_sa_pe" {
+  name                = "pe-sa-aidds-${var.environment}-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.asp_shrd_pe_subnet.id
+  tags                = module.tags.keyvalues
+
+  private_service_connection {
+    name                           = "psc-sa-aidds-${var.environment}-01"
+    private_connection_resource_id = azurerm_storage_account.aidds_sa.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name = "pdz-sa-aidds-${var.environment}-01"
+    private_dns_zone_ids = [
+      data.azurerm_private_dns_zone.blob.id
+    ]
+  }
+
+  custom_network_interface_name = "nic-pe-sa-aidds-${var.environment}-01"
+
+  depends_on = [azurerm_storage_account.aidds_sa]
+}
