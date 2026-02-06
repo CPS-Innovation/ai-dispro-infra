6e69599fec79152086706fed6d68fb0bbbfdfdd3
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-02-06 14:50:59 +0000

    Adding Prod TF Pipeline

--- terraform/azure-landing-zone/code/appServicePlan.tf
+++ terraform/azure-landing-zone/code/appServicePlan.tf
@@ -12,6 +12,6 @@ resource "azurerm_service_plan" "shared-asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "EP1"
+  sku_name            = "EP2"
   tags                = module.tags.keyvalues
 }
