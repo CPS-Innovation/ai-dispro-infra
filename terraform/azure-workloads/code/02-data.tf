4eb40eb521e4190738bbf84cb0e30d8b0c53adef
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-02-17 16:07:54 +0000

    Changing Location of Prod

--- terraform/azure-workloads/code/02-data.tf
+++ terraform/azure-workloads/code/02-data.tf
@@ -31,6 +31,8 @@ output "log_analytics_workspace_id" {
   value = data.azurerm_log_analytics_workspace.law.id
 }
 
+/*
+
 data "azurerm_app_service_plan" "shared_asp" {
   name                = "asp-aid-shrd-${var.subscription}-01"
   resource_group_name = "rg-aid-${var.subscription}-01"
@@ -40,6 +42,7 @@ output "app_service_plan_id" {
   value = data.azurerm_app_service_plan.shared_asp.id
 }
 
+*/
 data "azurerm_storage_account" "fadependency_sa" {
   name                = "stfadepaidshrd${var.subscription}01"
   resource_group_name = "rg-aid-${var.subscription}-01"
