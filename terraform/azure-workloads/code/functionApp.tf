83af241280e6791d3b62c0cf9c4aa2132ce49cc0
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-03-02 11:12:57 +0000

    Adding App Settings

--- terraform/azure-workloads/code/functionApp.tf
+++ terraform/azure-workloads/code/functionApp.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_function_app" "aid_func" {
     FUNCTIONS_EXTENSION_VERSION              = "~4"
     FUNCTIONS_WORKER_RUNTIME                 = "python"
     SCM_DO_BUILD_DURING_DEPLOYMENT           = "1"
-    WEBSITE_CONTENTSHARE                     = "fa-aid-${var.environment}-01"
+    WEBSITE_CONTENTSHARE                     = "fa-co-dispro-dev-01aa99"
     WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = data.azurerm_storage_account.fadependency_sa.primary_connection_string
     XDG_CACHE_HOME                           = "/tmp/.cache"
 
