9d608b89c56a67a7a5980d4a7fc96508e125e629
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-03-02 16:22:29 +0000

    Adding private endpoint to storage account

--- terraform/azure-landing-zone/code/appServicePlan.tf
+++ terraform/azure-landing-zone/code/appServicePlan.tf
@@ -7,6 +7,38 @@ resource "azurerm_storage_account" "fadependency-sa" {
   tags                     = module.tags.keyvalues
 }
 
+resource "azurerm_private_endpoint" "fadependency-sa-blob-pe" {
+  name                = "pe-sa-blob-aidfadep-${var.subscription}-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  subnet_id = lookup(
+    { for s in azurerm_virtual_network.vnet.subnet : s.name => s },
+    "snet-pe-prd-01"
+  ).id
+
+  tags = module.tags.keyvalues
+
+  private_service_connection {
+    name                           = "psc-sa-blob-aidfadep-${var.subscription}-01"
+    private_connection_resource_id = azurerm_storage_account.fadependency-sa.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name = "pdz-sa-blob-aidfadep-${var.subscription}-01"
+    private_dns_zone_ids = [
+      azurerm_private_dns_zone.blob.id
+    ]
+  }
+
+  custom_network_interface_name = "nic-pe-sa-blob-aidfadep-${var.subscription}-01"
+
+  depends_on = [azurerm_storage_account.fadependency-sa]
+}
+
+
 resource "azurerm_service_plan" "shared-asp" {
   name                = "asp-aid-shrd-${var.subscription}-01"
   resource_group_name = azurerm_resource_group.rg.name
